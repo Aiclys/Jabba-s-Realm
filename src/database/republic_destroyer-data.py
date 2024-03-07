c69028cda8d2ffa93e8c24b8fd0580bfcf213976
Aiclys <145050105+Aiclys@users.noreply.github.com>
2024-03-07 16:36:19 +0000

    add republic_destroyer file

---
+++ src/database/republic_destroyer-data.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import sqlite3
+
+con = sqlite3.connect("jabbas-data.db")
+cur = con.cursor()
+
+
+# DO NOT USE ANYMORE, tables already created
+def create_rep_dest_table():
+    rep_dest_table = """CREATE TABLE rep_dest(
+                name TEXT NOT NULL,
+                score INT,
+                highscore INT
+    )"""
+    cur.execute(rep_dest_table)
+
+def sort_users_by_score_asc():
+    result = cur.execute("SELECT username FROM rep_dest ORDER BY highscore ASC")
+    res = result.fetchall()
+    return res
+
+def sort_users_by_score_desc():
+    result = cur.execute("SELECT username FROM rep_dest ORDER BY highscore DESC")
+    res = result.fetchall()
+    return res
+
+def best_user():
+    result = cur.execute("SELECT username FROM rep_dest WHERE highscore=MAX(highscore)")
+    res = result.fetchall()
+    return res
+
+create_rep_dest_table()
+
+con.close()
