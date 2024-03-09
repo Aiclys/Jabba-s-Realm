b8202fe90b9ee33595ee2b3b5f1abba51e20dbba
Aiclys <145050105+Aiclys@users.noreply.github.com>
2024-03-09 17:16:27 +0100

    add comments to every function

--- src/database/republic_destroyer-data.py
+++ src/database/republic_destroyer-data.py
@@ -14,29 +14,34 @@ def create_rep_dest_table():
     )"""
     cur.execute(rep_dest_table)
 
+# Adds a player
 def add_player():
     name = input("Player name: ")
     score = int(input("Score: "))
     highscore = int(input("Highscore: "))
     cur.execute(f"INSERT INTO rep_dest VALUES('{name}', {score}, {highscore})")
 
-def sort_users_by_score_asc():
+# Sorts users by score in ascending order
+def sort_users_by_highscore_asc():
     result = cur.execute("SELECT username FROM rep_dest ORDER BY highscore ASC")
     res = result.fetchall()
     return res
 
-def sort_users_by_score_desc():
+# Sorts users by score in descending order
+def sort_users_by_highscore_desc():
     result = cur.execute("SELECT username FROM rep_dest ORDER BY highscore DESC")
     res = result.fetchall()
     return res
 
+# Ouputs the best user by highscore
 def best_user():
-    result = cur.execute("SELECT username FROM rep_dest WHERE highscore=MAX(highscore)")
+    result = cur.execute("SELECT username, highscore FROM rep_dest WHERE highscore=MAX(highscore)")
     res = result.fetchall()
     return res
 
+# Ouputs the worst user by highscore
 def worst_user():
-    result = cur.execute("SELECT username FROM rep_dest WHERE highscore=MIN(highscore)")
+    result = cur.execute("SELECT username, highscore FROM rep_dest WHERE highscore=MIN(highscore)")
     res = result.fethall()
     return res
 
