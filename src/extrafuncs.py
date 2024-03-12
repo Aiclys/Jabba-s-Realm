125182fdf863c56276a117f2c6c10b6b9c1aaea4
Aiclys <145050105+Aiclys@users.noreply.github.com>
2024-03-12 09:31:41 +0100

    Create extrafuncs.py

---
+++ src/extrafuncs.py
@@ -0,0 +1,13 @@
+import sqlite3
+import random
+
+
+def add_userid():
+    id_list = []
+
+
+def check_email(email_to_check):
+    acceptable_domains = ['gmail.com', 'outlook.com', 'outlook.de' 'gmx.de', 'gmx.net', 'nyrs.garden', 'protonmail.com', 'disroot.org', 'riseup.net']
+
+    if email_to_check[@::] in acceptable_domains:
+        return True
