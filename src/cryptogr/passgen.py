0a94e6f4f0f1d4361154b58ad9e59d73f9f2e93b
Aiclys <145050105+Aiclys@users.noreply.github.com>
2024-03-14 20:05:12 +0100

    create passgen.py for password generation and safety

---
+++ src/cryptogr/passgen.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import string
+import secrets
+import math
+
+def genpass(length):
+    all_characters = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(secrets.choice(all_characters) for i in range(length))
+    return password
+
+def genpassphrase(length):
+    return
+
+def password_entropy(password, numeric_form):
+    alphabet_lower = [letter for letter in string.ascii_letters][:26]
+    alphabet_upper = [letter for letter in string.ascii_letters][27:]
+    digits = [digit for digit in string.digits]
+    punctuation = [i for i in string.punctuation]
+    entropy_in_decimal = 1
+
+    for character in password:
+        if character in alphabet_lower:
+            entropy_in_decimal = entropy_in_decimal * 26
+        elif character in alphabet_upper:
+            entropy_in_decimal = entropy_in_decimal * 26
+        elif character in digits:
+            entropy_in_decimal = entropy_in_decimal * 10
+        elif character in punctuation:
+            entropy_in_decimal = entropy_in_decimal * 32
+
+    entropy_in_bits = math.log(entropy_in_decimal, 2)
+    entropy_in_bytes = entropy_in_bits / 8
+
+    if numeric_form == "decimal":
+        return entropy_in_decimal
+    elif numeric_form == "bits":
+        return entropy_in_bits
+    elif numeric_form == "bytes":
+        return entropy_in_bytes
+
+def password_safety(password):
+    if password_entropy(password, "bits") < 35 and password_entropy(password, "bits") < 50:
+        return "very weak"
+    elif password_entropy(password, "bits") >= 50 and password_entropy(password, "bits") < 75:
+        return "weak"
+    elif password_entropy(password, "bits") >= 75 and password_entropy(password, "bits") < 95:
+        return "medium"
+    elif password_entropy(password, "bits") >= 95 and password_entropy(password, "bits") < 125:
+        return "strong"
+    elif password_entropy(password, "bits") >= 125:
+        return "very strong"
